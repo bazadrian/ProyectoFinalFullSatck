f912d1358cd7f486df3ef53770ebcfd99b73cb0a
Adrian Bazan <adrian@bazancloud.com>
2024-07-11 20:22:52 -0600

    CRUD de Movies

--- backend/controllers/moviesControllers.js
+++ backend/controllers/moviesControllers.js
@@ -2,9 +2,21 @@ const getMovies = (req, res) => {
     res.status(200).json({ message: 'getMovies'})
 }
 
+const crearMovies = (req, res) => {
+    res.status(201).json({ message: 'crearMovies'})
+}
+
+const updateMovies = (req, res) => {
+    res.status(200).json({ message: 'Modificar la movie ${req.params.id}'})
+}
+
+const deleteMovies = (req, res) => {
+    res.status(200).json({ message: 'Borraste la movie ${req.params.id}'})
+}
+
 module.exports = {
-    getMovies
-   // crearMovies,
-    // updateMovies,
-    // deleteMovies
+    getMovies,
+    crearMovies,
+    updateMovies,
+    deleteMovies
 }
--- backend/routes/moviesRoutes.js
+++ backend/routes/moviesRoutes.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const router = express.Router()
-const { getMovies } = require ('../controllers/moviesControllers')
+const { getMovies, crearMovies, updateMovies, deleteMovies } = require ('../controllers/moviesControllers')
 
 router.get('/', getMovies)
+router.post('/', crearMovies)
+
+router.put('/:id', updateMovies)
+router.delete('/', deleteMovies)
 
 module.exports = router
